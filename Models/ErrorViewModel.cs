f5d791324affc926ddc0b6926df6978c753137fa
Promise <103519309+Sthe-Prom@users.noreply.github.com>
2024-11-15 12:04:28 +0200

    Init Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Digitalisation_Funding.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
